fd193af4677ec041e001f18586594a69a36cfcd5
javidselimov12345 <javidzs@code.edu.az>
2022-12-20 13:22:48 +0400

    connected db to express

--- server/index.js
+++ server/index.js
@@ -1,13 +1,43 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "12345",
+  database: "web4",
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    res.send('Are you ready')
+app.get("/", (req, res) => {
+  res.send("Are you ready");
+});
+
+//  connecet db and read data endpoint
+
+
+
+app.get('/playlist', (req, res) => {
+    const command = `select * from musiclist`;
+    conn.query(command, (err,data) => {
+        if (!err) res.status(200).json(data)
+        res.status(404).send("Error not found")
+    })
+})
+
+
+conn.connect((err) => {
+    if(!err) console.log("db is connected")
 })
 
 app.listen(PORT, (err) => {
-    if(!err) console.log(`server is runnig on port - ${PORT}`)
-})
+  if (!err) console.log(`server is runnig on port - ${PORT}`);
+});
